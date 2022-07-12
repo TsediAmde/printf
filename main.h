7a47bf3a69da9028d93758b7c1db8443e627ace8
TsediAmde <tsedeniaamde92@gmail.com>
2022-07-12 08:51:44 -0700

    frist commit

--- main.h
+++ main.h
@@ -1,44 +1,34 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
 
-/**
-* struct convert - defines a structure for symbols and functions
-* @sym: The operator
-* @f: The function associated
-*/
-struct convert
-{
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct convert conver_t;
-
-/*Main functions*/
-int parser(const char *format, conver_t f_list[], va_list arg_list);
+int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 int _printf(const char *format, ...);
-int _write_char(char);
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_integer(va_list);
-int print_number(va_list);
-int print_binary(va_list);
-int print_reversed(va_list arg);
-int rot13(va_list);
-int unsigned_integer(va_list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_heX(va_list list);
+void print_number(int n, int *count);
 
-/*Helper functions*/
-unsigned int base_len(unsigned int, int);
-char *rev_string(char *);
-void write_base(char *str);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int print_unsgined_number(unsigned int);
+/**
+ * struct vartype - struct vartype
+ *
+ * @vartype: type of variable to be printed
+ * @f: the function associated
+ */
 
+typedef struct vartype
+{
+	char *vartype;
+	int (*f)(va_list);
+} var_t;
+
+int c_func(va_list args);
+int s_func(va_list args);
+int perc_func(va_list args);
+int i_func(va_list args);
+int d_func(va_list args);
+int b_func(va_list args);
+int rev_func(va_list args);
+int rot_func(va_list args);
 #endif
