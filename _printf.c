e449af01dc14a031f4342b14081ce1fea0215f67
tilahunmichael <95909737+tilahunmichael@users.noreply.github.com>
2022-07-12 20:01:14 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@
  * _printf - Receives the main string and all the necessary parameters to
  * print a formated string
  * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
+ * Return: A total count of the character printed
  */
 int _printf(const char *format, ...)
 {
